40b66d106eb987a608724c736e2c3815ddd1d08a
EddieJorden <eddie@eddiejorden.tech>
2022-08-17 17:42:05 -0700

    styled container created

---
+++ src/components/ComponentContainer.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  background-color: #3e505c;
+  padding: 40px;
+  border-radius: 10px;
+  font-weight: heavy;
+  width: 300px;
+  height: 180px;
+`;
+
+export default StyledContainer;
