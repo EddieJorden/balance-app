51a1695a7e0e715c1c46c2ae1ad9916dbc0205e4
EddieJorden <eddie@eddiejorden.tech>
2023-01-04 19:56:52 -0800

    added mission statement, possibly added or upgraded some modules or packages

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -16,6 +16,19 @@ function contentToDisplay() {
   }
   return (
     <div>
+      <div style={{ margin: '23px' }}>
+        Our mission at Balance App is to ignite a revolution in
+        the way we live, work, and relate to the world around us. We
+        believe that it is time to draw a line in the sand and say
+        enough is enough - we will not accept a society that values
+        money and overwork above all else, and that destroys the
+        well-being and happiness of individuals, families, and
+        communities. We are here to offer a powerful and intuitive
+        interface that helps people manage their tasks, schedule,
+        and time more effectively, and to reclaim the balance,
+        joy, and purpose that have been lost in the pursuit of
+        profit. Join us, and let&apos;s build a better world together.
+      </div>
       <UserLogin />
     </div>
   );
