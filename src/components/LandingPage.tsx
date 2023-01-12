0daa79c76c30c18e6c66cae784152267a9545d01
EddieJorden <eddie@eddiejorden.tech>
2023-01-12 10:21:36 -0800

    some minor adjustments

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,7 +4,7 @@ import UserLogin from './UserLogin';
 import { selectIsNewUser } from './userSlice';
 import UserLandingPage from './UserLandingPage';
 import ChatWindow from './AiComponent';
-import { prefix } from './utils';
+import { missionStatement, prefix } from './utils';
 
 function contentToDisplay() {
   const isNewUser = useSelector(selectIsNewUser);
@@ -20,17 +20,7 @@ function contentToDisplay() {
   return (
     <div>
       <div style={{ margin: '23px' }}>
-        Our mission at Balance App is to ignite a revolution in
-        the way we live, work, and relate to the world around us. We
-        believe that it is time to draw a line in the sand and say
-        enough is enough - we will not accept a society that values
-        money and overwork above all else, and that destroys the
-        well-being and happiness of individuals, families, and
-        communities. We are here to offer a powerful and intuitive
-        interface that helps people manage their tasks, schedule,
-        and time more effectively, and to reclaim the balance,
-        joy, and purpose that have been lost in the pursuit of
-        profit. Join us, and let&apos;s build a better world together.
+        {missionStatement}
       </div>
       <ChatWindow apiUrl={apiUrl} />
       <div />
