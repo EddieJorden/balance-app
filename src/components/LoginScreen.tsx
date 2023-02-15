878c0066434a40b89835b053eb1ab4e17e96f310
EddieJorden <eddie@eddiejorden.tech>
2023-02-15 11:58:55 -0800

    some slight css changes to the login component

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -4,72 +4,92 @@ import { useDispatch } from 'react-redux';
 import { updateUserProfile } from './userProfileSlice';
 
 const StyledLoginContainer = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100vh;
-background-color: #1c1c1e;
-color: white;
-
-form {
-background-color: #2f2f31;
-padding: 40px;
-border-radius: 10px;
-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
-width: 500px;
-display: flex;
-flex-direction: column;
-align-items: center;
-animation: fadeIn 0.5s ease-in-out;
-
-@keyframes fadeIn {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-h2 {
-  margin-bottom: 30px;
-}
-
-input[type="text"],
-input[type="email"] {
-  width: 100%;
-  padding: 15px;
-  margin-bottom: 20px;
-  border: none;
-  border-radius: 5px;
-  background-color: #3a3a3c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  min-width: 100vw;
+  background-color: #1c1c1e;
   color: white;
-  font-size: 18px;
-  transition: all 0.2s ease-in-out;
 
-  &:focus {
-    outline: none;
-    box-shadow: 0px 0px 10px 0px rgba(255, 255, 255, 0.5);
+  form {
+    background-color: #2f2f31;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+    max-width: 520px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    animation: fadeIn 0.5s ease-in-out;
+
+    @media (max-width: 380px) {
+      form {
+        width: auto;
+        height: auto;
+        min-height: 100vh;
+      }
+    }
+
+    @media (max-width: 767px) {
+      form {
+        width: 100%;
+        max-width: 100%;
+      }
+    }
+    
+    
+
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+
+    h2 {
+      margin-bottom: 30px;
+    }
+
+    input[type="text"],
+    input[type="email"] {
+      width: 100%;
+      padding: 15px;
+      margin-bottom: 20px;
+      border: none;
+      border-radius: 5px;
+      background-color: #3a3a3c;
+      color: white;
+      font-size: 18px;
+      transition: all 0.2s ease-in-out;
+
+      &:focus {
+        outline: none;
+        box-shadow: 0px 0px 10px 0px rgba(255, 255, 255, 0.5);
+      }
+    }
+
+    input[type="submit"] {
+      width: 100%;
+      padding: 15px;
+      margin-top: 30px;
+      border: none;
+      border-radius: 5px;
+      background-color: #00c853;
+      color: white;
+      font-size: 18px;
+      cursor: pointer;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        background-color: #1de9b6;
+      }
+    }
   }
-}
-
-input[type="submit"] {
-  width: 100%;
-  padding: 15px;
-  margin-top: 30px;
-  border: none;
-  border-radius: 5px;
-  background-color: #00c853;
-  color: white;
-  font-size: 18px;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-
-  &:hover {
-    background-color: #1de9b6;
-  }
-}
-}
 `;
 
 function LoginScreen() {
