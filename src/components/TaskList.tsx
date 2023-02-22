2853cf11e708aa9ffa8761d0fbe2da6879a855cc
EddieJorden <eddie@eddiejorden.tech>
2023-02-21 17:24:50 -0800

    adding add new task functionality

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import TaskComponent from './TaskComponent';
@@ -21,8 +22,21 @@ function TaskList() {
     return (
       <div>
         <h2>Tasks</h2>
-        <ul className="task-list">
+        <form>
+          <label htmlFor="taskName">Task Name</label>
+          <input type="text" id="taskName" name="taskName" />
+          <label htmlFor="taskDescription">Task Description</label>
+          <input type="text" id="taskDescription" name="taskDescription" />
+          <label htmlFor="taskDueDate">Task Due Date</label>
+          <input type="text" id="taskDueDate" name="taskDueDate" />
+          <label htmlFor="taskPriority">Task Priority</label>
+          <input type="text" id="taskPriority" name="taskPriority" />
+          <label htmlFor="taskStatus">Task Status</label>
+          <input type="text" id="taskStatus" name="taskStatus" />
+          <button type="submit">Create Task</button>
+        </form>
 
+        <ul className="task-list">
           {tasks.map((task: Task) => (
             <TaskComponent key={task.id} task={task} />
           ))}
@@ -30,12 +44,12 @@ function TaskList() {
       </div>
     );
   }
+
   return (
     <div>
       <h2>Tasks</h2>
       <ul className="task-list">
-
-        No tasks
+        <li>No tasks</li>
       </ul>
     </div>
   );
